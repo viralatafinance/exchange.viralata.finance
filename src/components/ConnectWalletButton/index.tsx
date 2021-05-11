d26e175a2a2aea1de8ff23ef054c087c9cc3c3d9
- <->
2021-05-11 15:15:38 -0300

    CBRL + unlock wallet bug

--- src/components/ConnectWalletButton/index.tsx
+++ src/components/ConnectWalletButton/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { ButtonProps, ConnectorNames } from '@pancakeswap-libs/uikit'
+import { useModal } from '@geist-ui/react'
+
 import { useWalletModal } from 'components/WalletModal'
+import ConnectModal from 'components/WalletModal/ConnectModal'
+import AccountModal from 'components/WalletModal/AccountModal'
 import { connectorsByName } from 'connectors'
 import useI18n from 'hooks/useI18n'
 import ButtonCTA from 'components/ButtonCTA'
@@ -9,6 +13,8 @@ import ButtonCTA from 'components/ButtonCTA'
 const UnlockButton: React.FC<ButtonProps> = (props) => {
   const TranslateString = useI18n()
   const { account, activate, deactivate } = useWeb3React()
+  const connectModal = useModal(false)
+  const accountModal = useModal(false)
 
   const handleLogin = (connectorId: ConnectorNames) => {
     const connector = connectorsByName[connectorId]
@@ -16,10 +22,23 @@ const UnlockButton: React.FC<ButtonProps> = (props) => {
       activate(connector)
     }
   }
-
-  const { onPresentConnectModal } = useWalletModal(handleLogin, deactivate, account as string)
-
-  return <ButtonCTA onClick={onPresentConnectModal}>{TranslateString(292, 'Unlock Wallet')}</ButtonCTA>
+  return (
+    <>
+      <ConnectModal isOpen={connectModal.visible} onDismiss={() => connectModal.setVisible(false)} login={handleLogin} />
+      <AccountModal isOpen={accountModal.visible} onDismiss={() => accountModal.setVisible(false)} account={account || ''} logout={deactivate} />
+      <ButtonCTA
+        onClick={() => {
+          if (account) {
+            accountModal.setVisible(true)
+          } else {
+            connectModal.setVisible(true)
+          }
+        }}
+      >
+        {TranslateString(292, 'Unlock Wallet')}
+      </ButtonCTA>
+    </>
+  )
 }
 
 export default UnlockButton
